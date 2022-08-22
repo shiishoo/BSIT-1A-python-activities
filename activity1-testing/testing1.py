bf701ae7ede9bd35bf75c7933f850f7faf906318
shiishoo <ontingcohershey@gmail.com>
2022-08-22 21:06:49 +0800

    Create testing1.py

---
+++ activity1-testing/testing1.py
@@ -0,0 +1,23 @@
+from ast import If
+
+
+print("ito ang activity 1");
+a = 2
+b = 2
+c = 1
+d = 3
+if a != b :
+    print("bleh");
+    if b < c:
+        print("no");
+        print("mali to");
+    
+    print("oks to");
+if a == d:
+    print("uwu");
+elif c==a:
+    print("djhsgfds");
+if b < d:
+    print("sheesh");
+else:
+    print("else to");
